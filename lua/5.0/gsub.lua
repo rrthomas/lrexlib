1f66da5515c4e993cd7f6163218e08bb57a50a5f
rrt <rrt>
2006-03-20 22:48:32 +0000

    New version from Shmuel

--- lua/5.0/gsub.lua
+++ lua/5.0/gsub.lua
@@ -4,41 +4,40 @@
   it allows to plug in various search/match  engines (such
   as PCRE or POSIX regex library).
 
-  Shmuel Zeigerman; June 2004 - November 2005.
+  Shmuel Zeigerman; June 2004 - February 2006.
 --]]
 
 local insert = table.insert
 local sub = string.sub
 -----------------------------------------------------------------
 -- Split the "replace" string and store parts into a table.
---   a) Replace %x with tonumber(x) (if x matches [0-9])
+--   a) Replace %x with tonumber(x) (if x matches [1-9])
 --      and store those numbers ("captures") as separate items
 --      in the table.
 --      (The %x'es described in this paragraph are the text
 --      splitting points).
---   b) Replace %x with x (if x does not match [0-9])
---   c) Return the table and the number of "captures".
+--   b) Replace %x with x (if x does not match [1-9])
+--   c) Return the table.
 -----------------------------------------------------------------
 local function split_rep_string(str)
     local tb = {}
-    local num_capt = 0
     local chunk_start = 1
-    for s,char in string.gmatch(str, "()%%(.)") do
-        if string.find(char, "[0-9]") then
+    for s,char in string.gfind(str, "()%%(.)") do
+        assert(char ~= "0", "invalid capture index")
+        if string.find(char, "[1-9]") then
             local chunk = sub(str, chunk_start, s-1)
             if chunk ~= "" then
                 insert(tb, (string.gsub(chunk, "%%(.)", "%1")))
             end
             insert(tb, tonumber(char))
             chunk_start = s+2
-            num_capt = num_capt + 1
         end
     end
     local chunk = sub(str, chunk_start)  -- store the last chunk
     if chunk ~= "" then
         insert(tb, (string.gsub(chunk, "%%(.)", "%1")))
     end
-    return tb, num_capt
+    return tb
 end
 
 -- generic_gsub()
@@ -49,7 +48,7 @@ end
 --       end
 --   Wrapper like this can be direct replacement for string.gsub().
 --
---   This function's API is meant to be 100% string.gsub (Lua 5.1)
+--   This function's API is meant to be 100% string.gsub (Lua 5.02)
 --   compatible,
 --   except for syntax of regular expression in the argument 'pat'
 --   which must correspond to the library determined by argument 'func'.
@@ -61,8 +60,9 @@ end
 --   is a trailing one) or passed as nil.
 --
 function generic_gsub (func, str, pat, repl, n, cflags, eflags)
-    assert(type(repl) == "string" or type(repl) == "function" or type(repl) == "table",
-                "generic_gsub: 4th argument must be string, function or table")
+    local reptype = type(repl)
+    assert(reptype == "string" or reptype == "function",
+                "generic_gsub: 4th argument must be string or function")
     assert(n == nil or type(n) == "number",
                 "generic_gsub: 5th argument must be number or nil")
 
@@ -71,68 +71,51 @@ function generic_gsub (func, str, pat, repl, n, cflags, eflags)
     local curr_start = 1           -- current offset in str for matching operation
     local tb_out = {}              -- array of the output string's parts
     local tb_replace               -- array of the replace string's parts
-    local n_capt_repl              -- number of captures in the replace string
 
-    if type(repl) == "string" then
-        tb_replace, n_capt_repl = split_rep_string(repl)
+    if reptype == "string" then
+        tb_replace = split_rep_string(repl)
     end
 
     while not n or n > num_rep do
 
-        local s,e,t = expr:match(str, curr_start, eflags)
-        if s then
-            insert( tb_out, sub(str, curr_start, s-1) )
+        local from,to,cap = expr:match(str, curr_start, eflags)
+        if not from then break; end
+        insert( tb_out, sub(str, curr_start, from-1) )
 
-            if type(repl) == "string" then
-                if n_capt_repl == 0 then
-                    insert(tb_out, tb_replace[1])
+        if reptype == "string" then
+            for _,v in ipairs(tb_replace) do
+                if type(v) == "number" then
+                    assert(cap[v] ~= nil, "invalid capture index")
+                    if cap[v] then insert(tb_out, cap[v]) end
                 else
-                    for _,v in ipairs(tb_replace) do
-                        if type(v) == "number" then
-                            if v == 0 then insert(tb_out, sub(str,s,e))
-                            elseif t[v] then insert(tb_out, t[v])
-                            end
-                        else
-                            insert(tb_out, v)
-                        end
-                    end
-                end
-
-            elseif type(repl) == "function" then
-                local val
-                if #t > 0 then
-                    val = repl(unpack(t))
-                else
-                    val = repl(sub(str,s,e))
-                end
-                if type(val) == "string" then
-                    insert(tb_out, val)
-                else--if not val then
-                    insert(tb_out, sub(str,s,e))
-                end
-
-            elseif type(repl) == "table" then
-                local val
-                if t[1] then
-                    val = repl[t[1]]
-                else
-                    val = repl[sub(str,s,e)]
-                end
-                if type(val) == "string" then
-                    insert(tb_out, val)
-                else--if not val then
-                    insert(tb_out, sub(str,s,e))
+                    insert(tb_out, v)
                 end
             end
 
-            num_rep = num_rep + 1
-            curr_start = e+1
+        else -- if reptype == "function" then
+            local val
+            if table.getn(cap) > 0 then
+                val = repl(unpack(cap))
+            else
+                val = repl(sub(str,from,to))
+            end
+            if type(val) == "string" then
+                insert(tb_out, val)
+            end
+        end
+
+        num_rep = num_rep + 1
+        if curr_start <= to then
+            curr_start = to+1
+        elseif curr_start <= string.len(str) then -- advance by 1 char (not replaced)
+            table.insert(tb_out, string.sub(str, curr_start, curr_start))
+            curr_start = curr_start + 1
         else
             break
         end
     end
 
-    if curr_start <= #str then
+    if curr_start <= string.len(str) then
         insert(tb_out, sub(str, curr_start))
     end
 
