8af4a80821486b43062b47edcc47336e8712b001
Reuben Thomas <rrt@sc3d.org>
2010-07-30 18:20:59 +0100

    Add GNU-specific test (of translation tables).

---
+++ test/gnu_sets.lua
@@ -0,0 +1,38 @@
+-- See Copyright Notice in the file LICENSE
+
+-- This file should contain only test sets that behave identically
+-- when being run with pcre or posix regex libraries.
+
+local luatest = require "luatest"
+local N = luatest.NT
+
+local function norm(a) return a==nil and N or a end
+
+local function set_f_gmatch (lib, flg)
+  local downcase = {}
+  for i = 0, 255 do -- 255 == UCHAR_MAX
+    downcase[i] = string.gsub(string.char (i), ".", function (s) return string.lower(s) end)
+  end
+  -- gmatch (s, p, [cf], [ef], [tr])
+  local function test_gmatch (subj, patt)
+    local out, guard = {}, 10
+    for a, b in lib.gmatch (subj, patt, nil, nil, downcase) do
+      table.insert (out, { norm(a), norm(b) })
+      guard = guard - 1
+      if guard == 0 then break end
+    end
+    return unpack (out)
+  end
+  return {
+    Name = "Function gmatch",
+    Func = test_gmatch,
+  --{  subj             patt         results }
+    { {"abA",           "a"},        {{"a",N}, {"A",N} } },
+  }
+end
+return function (libname)
+  local lib = require (libname)
+  return {
+    set_f_gmatch    (lib),
+  }
+end
