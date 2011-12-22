7836c34663ca4ae78b0e39913cb5a5f5f37c2024
Shmuel Zeigerman <solomuz0@gmail.com>
2011-12-22 18:48:57 +0200

    lpcre_f.c: added flags up to PCRE version 8.21

--- src/pcre/lpcre_f.c
+++ src/pcre/lpcre_f.c
@@ -106,6 +106,12 @@ static flag_pair pcre_flags[] = {
 #ifdef PCRE_INFO_MINLENGTH
   { "INFO_MINLENGTH",                PCRE_INFO_MINLENGTH },
 #endif
+#ifdef PCRE_INFO_JIT
+  { "INFO_JIT",                      PCRE_INFO_JIT },
+#endif
+#ifdef PCRE_INFO_JITSIZE
+  { "INFO_JITSIZE",                  PCRE_INFO_JITSIZE },
+#endif
 /*---------------------------------------------------------------------------*/
 #if VERSION_PCRE >= 400
   { "EXTRA_STUDY_DATA",              PCRE_EXTRA_STUDY_DATA },
